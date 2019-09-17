93b56e25f85c80a1fbd6decc01d1558c95bf178a
luckyboy <mikka_tolmachev@protonmail.com>
2019-09-18 01:47:29 +0300

    calculator.py оформлен по Pep-8

--- calculator.py
+++ calculator.py
@@ -1,38 +1,47 @@
-def division_zero(first_number,second_number):
+def division_zero(first_number, second_number):
     """Обрабатывает запрос на деление на 0"""
     try:
         return first_number / second_number
     except ZeroDivisionError:
-        return ("Деление на 0 не поддерживается")
-def mod_division_zero (first_number,second_number):
+        return "Деление на 0 не поддерживается"
+
+
+def mod_division_zero(first_number, second_number):
     """Обрабатывает запрос на деление на 0"""
     try:
         return first_number % second_number
     except ZeroDivisionError:
-        return ("Деление на 0 не поддерживается")
-def div_division_zero (first_number,second_number):
+        return "Деление на 0 не поддерживается"
+
+
+def div_division_zero(first_number, second_number):
     """Обрабатывает запрос на деление на 0"""
     try:
         return first_number // second_number
     except ZeroDivisionError:
-        return ("Деление на 0 не поддерживается")
+        return "Деление на 0 не поддерживается"
+
+
 class Calculator():
     def __init__(self, first_number, second_number, action):
         self.first_number = first_number
         self.second_number = second_number
         self.action = action
-        self.actions = {'+': self.first_number+self.second_number, '-': self.first_number-self.second_number,
-                        '*': self.first_number*self.second_number, 'pow': self.first_number**self.second_number,
-                        '/': division_zero(self.first_number,self.second_number), 'mod': mod_division_zero(self.first_number, self.second_number),
+        self.actions = {'+': self.first_number + self.second_number, '-': self.first_number - self.second_number,
+                        '*': self.first_number * self.second_number, 'pow': self.first_number ** self.second_number,
+                        '/': division_zero(self.first_number, self.second_number),
+                        'mod': mod_division_zero(self.first_number, self.second_number),
                         'div': div_division_zero(self.first_number, self.second_number)}
+
     def calculate(self):
         try:
             return self.actions[self.action]
         except KeyError:
-            return ("Cписок команд, поддерживаемых калькулятором c числами: + - * / pow  mod div")
+            return "Cписок команд, поддерживаемых калькулятором c числами: + - * / pow mod div"
 
-first_number = float(input())
-action = str(input())
-second_number = float(input())
-calculator = Calculator(first_number=first_number,second_number= second_number,action= action)
+
+first_number = float(input('Введите первое число: '))
+action = input('Введите арифметическое действие: ')
+second_number = float(input('Введите второе число: '))
+calculator = Calculator(first_number=first_number, second_number=second_number, action=action)
 print(calculator.calculate())
