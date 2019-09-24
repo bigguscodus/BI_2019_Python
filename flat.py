c627ed3d36ea0edbb31029d5f15a274d668fc097
bigguscodus <mikka_tolmachev@protonmail.com>
2019-09-24 22:25:30 +0300

    Подогнана длина строки

--- flat.py
+++ flat.py
@@ -1,5 +1,4 @@
 def flat_list(array):
-    """Recursive function for making nested lists flatter. Be aware of the depth"""
     flat = []
     for i in array:
         if type(i) == type(flat):
@@ -8,10 +7,3 @@ def flat_list(array):
             flat.append(i)
     return flat
 
-
-if __name__ == '__main__':
-    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
-    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
-    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
-    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
-    print('Done! Check it')
--- non_uniq.py
+++ non_uniq.py
@@ -1,10 +1,3 @@
 def checkio(data):
-    """This function returns non unique elements of the given list"""
     return [i for i in data if data.count(i) > 1]
 
-
-if __name__ == "__main__":
-    # These "asserts" using only for self-checking and not necessary for auto-testing
-    assert list(checkio([1, 2, 3, 1, 3])) == [1, 3, 1, 3], "1st example"
-    assert list(checkio([1, 2, 3, 4, 5])) == [], "2nd example"
-    assert list(checkio([5, 5, 5, 5, 5])) == [5, 5, 5, 5, 5], "3rd example"
