56ec04d766daafc64f1d630517f87f4800448735
bigguscodus <mikka_tolmachev@protonmail.com>
2020-03-04 01:59:09 +0300

    Add utility functions

--- filter_fastqc.py
+++ filter_fastqc.py
@@ -9,7 +9,18 @@ parser.add_argument("--fastq", required=True, help="Path to fastq file for filte
 parser.add_argument("--output_base_name", help="Prefix for names of the created files")
 args = parser.parse_args()
 
+def _check_gc_content(read: list, minimum: int, maximum: int):
+    """Filter by GC content. Including soft masking fastqc"""
+    gc_percent = 100 * (read[1].count("G") + read[1].count("g") + read[1].count("C") + read[1].count("c")) / len(
+        read[1])
+    if minimum < gc_percent < maximum:
+        return True
+    return False
 
 
+def _check_length(read: list, threshhold: int):
+    if len(read[1]) > threshhold:
+        return True
+    return False
 
 filter_fastqc()
